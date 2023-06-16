abc94559ef8e1295d6693abaccb7588e30acde9b
Christian_Cardenas <ch.cardenasi@duocuc.cl>
2023-06-16 15:20:34 -0400

    iva

--- actividad2Funciones.py
+++ actividad2Funciones.py
@@ -1,53 +1,51 @@
 
-import sys
 
-def calcular_iva():
-    precio = float(input("Ingrese el precio del producto: "))
+
+def calcular_iva(precio):
     iva = precio * 0.19
-    precio_total = precio - iva
-    print(f"El precio total sin iva es: {precio_total}")
-    print(f"el iva aplicado es  : {iva}")
+    total_con_iva = precio + iva
+    return iva, total_con_iva
 
+def calcular_descuento(precio, descuento):
+    total_descuento = precio - descuento
+    return descuento, total_descuento
 
-def aplicar_descuento():
-    precio = float(input("Ingrese el precio del producto: "))
-    descuento = float(input("Ingrese el porcentaje de descuento: "))
-    descuento_decimal = descuento / 100
-    precio_descuento = precio - (precio * descuento_decimal)
-    print(f"El precio con el descuento aplicado es: {precio_descuento:.2f}")
-
-
-def calcular_imc():
-    peso = float(input("Ingrese su peso en kilogramos: "))
-    altura = float(input("Ingrese su altura en metros: "))
+def calcular_imc(peso, altura):
     imc = peso / (altura ** 2)
-    print(f"Su índice de masa corporal (IMC) es: {imc:.2f}")
+    return imc
 
 def mostrar_menu():
-    print("Selecciona una opción:")
-    print("1. Calcular IVA")
-    print("2. Aplicar descuento")
+    print("Menú:")
+    print("1. Calcular IVA ")
+    print("2. Calcular Descuento ")
     print("3. Calcular IMC")
     print("4. Salir")
 
-    opcion = input("Ingresa el número de opción: ")
-    return opcion
-
 def main():
     while True:
-        opcion = mostrar_menu()
+        mostrar_menu()
+        opcion = int(input("Ingrese el número de la opción que desea ingresar: "))
 
-        if opcion == '1':
-            calcular_iva()
-        elif opcion == '2':
-            aplicar_descuento()
-        elif opcion == '3':
-            calcular_imc()
-        elif opcion == '4':
-            sys.exit()
+        if opcion == 1:
+            precio = float(input("Ingrese el precio del producto: "))
+            iva, total_con_iva = calcular_iva(precio)
+            print("El valor del IVA es:", iva)
+            print("El total del producto con IVA es:", total_con_iva)
+        elif opcion == 2:
+            precio = float(input("Ingrese el precio del producto: "))
+            descuento = float(input("Ingrese el porcentaje de descuento: "))
+            descuento_decimal = descuento / 100
+            precio_descuento = precio - (precio * descuento_decimal)
+            print("El precio con el descuento aplicado es:", precio_descuento)
+        elif opcion == 3:
+            peso = float(input("Ingrese su peso en kilogramos: "))
+            altura = float(input("Ingrese su altura en metros: "))
+            imc = calcular_imc(peso, altura)
+            print("Su índice de masa corporal (IMC) es:", imc)
+        elif opcion == 4:
+            print("Usted a salido del programa. ")
+            break
         else:
-            print("Opción inválida. Por favor, selecciona una opción válida.\n")
+            print("Opción inválida. Por favor, ingrese un número válido.")
 
-if __name__ == '__main__':
-    main()
-
+main()
